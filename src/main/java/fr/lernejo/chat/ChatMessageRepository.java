d5bb158f94dfc61ccb5683aee1310030396348fb
Geatfine <david.houssian@gmail.com>
2021-12-09 19:31:20 +0100

    multi message reading done

---
+++ src/main/java/fr/lernejo/chat/ChatMessageRepository.java
@@ -0,0 +1,23 @@
+package fr.lernejo.chat;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ChatMessageRepository {
+    private final List<String> messages;
+
+    public ChatMessageRepository() {
+        messages = new ArrayList<>();
+    }
+
+    void addChatMessage(String message) {
+        messages.add(message);
+    }
+
+    List<String> getLastTenMessages() {
+        return messages.subList(Math.max(0, messages.size() - 10), messages.size());
+    }
+}
